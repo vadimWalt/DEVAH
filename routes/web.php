a70dc49ff4bc69d47248c43e8b910ea107a0918a
h3ska <alaskar.hamidi@gmail.com>
2023-08-23 19:14:48 +0200

    I hate Kracken

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 /*
@@ -16,9 +17,7 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return view('test');
-});
+Route::get('/test', [CourseController::class, 'index']);
 
 
 // Show register form
