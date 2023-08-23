f56f41cf898a79ca28a37584833eb977e83a3b55
Aza <amisipehr@gmail.com>
2023-08-23 15:30:26 +0200

    cards and course cards

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -5,16 +5,15 @@
         {{-- if we have an image link in the table : show the image --}}
         {{-- otherwise, show the default image --}}
         <img class="hidden w-48 mr-6 md:block"
-            src="{{ $course->logo ? asset('storage/' . $course->logo) : asset('images/no-image.png') }}" alt="" />
+            src="{{ $course->image ? asset('storage/' . $course->image) : asset('images/no-image.png') }}"
+            alt="" />
         <div>
             <h3 class="text-2xl">
                 <a href="/courses/{{ $course->id }}">{{ $course->title }}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{ $course->tutor }}</div>
             <x-course-tags :tagsCsv="$course->tags" />
-            <div class="text-lg mt-4">
-                <i class="fa-solid fa-location-dot"></i> {{ $course->location }}
-            </div>
+            <div class="text-xl font-bold mb-4">{{ $course->description }}</div>
         </div>
     </div>
 </x-card>
