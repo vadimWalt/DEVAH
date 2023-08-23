2002fecfe81d70bf82263154382aa3e2c77fe65a
EstherImamun <imams1234.et@gmail.com>
2023-08-23 12:29:33 +0200

    this is just a ramdom change no biggie

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -41,7 +41,7 @@
         
         <div class="devah">
             <a href="#" class="text-white">
-                <i class="fa-solid fa-school fa-2xl" style="color: #511f46;"></i>            </a>
+                <i class="fa-solid fa-school fa-2xl" style="color: #ffccff;"></i>            </a>
             DEVAH ACADEMY</div>
         <div class="hidden md:flex space-x-4">
           <a href="#" class="text-white">Home</a>
