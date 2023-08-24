ba799f4689f1e381907973ac98068899475153af
h3ska <alaskar.hamidi@gmail.com>
2023-08-24 09:52:35 +0200

    added address fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+    /* protected $fillable = [
         'name',
         'email',
         'password',
@@ -31,7 +31,7 @@ class User extends Authenticatable
         'street',
         'country',
         'role',
-    ];
+    ]; */
 
     /**
      * The attributes that should be hidden for serialization.
