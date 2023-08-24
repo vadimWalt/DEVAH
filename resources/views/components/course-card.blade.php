e15480bfa04042dae5c92d3bcb3b8375caa07236
Aza <amisipehr@gmail.com>
2023-08-24 09:59:51 +0200

    updated cards

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,15 +1,10 @@
 @props(['course'])
 
-
-<!-- component -->
-<div class="min-h-screen bg-gradient-to-tr from-red-300 to-yellow-200 flex justify-center items-center py-20">
-    <div class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
+<div class="min-h-screen bg-gray-500 py-20">
+    <h2 class="text-3xl text-center mb-8">New Courses</h2> <!-- Added heading here -->
+    <div class="container mx-auto md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
         <x-card />
         <x-card />
         <x-card />
-
-
-
-
     </div>
 </div>
