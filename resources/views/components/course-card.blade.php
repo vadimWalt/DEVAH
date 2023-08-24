4ff1122436e2a32463175a7240521b77e40316b0
Aza <amisipehr@gmail.com>
2023-08-24 09:03:28 +0200

    card and course-card

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,19 +1,15 @@
 @props(['course'])
 
-<x-card class="fixed bottom-4">
-    <div class="flex">
-        {{-- if we have an image link in the table : show the image --}}
-        {{-- otherwise, show the default image --}}
-        <img class="hidden w-48 mr-6 md:block"
-            src="{{ $course->image ? asset('storage/' . $course->image) : asset('images/no-image.png') }}"
-            alt="" />
-        <div>
-            <h3 class="text-2xl">
-                <a href="/courses/{{ $course->id }}">{{ $course->title }}</a>
-            </h3>
-            <div class="text-xl font-bold mb-4">{{ $course->tutor }}</div>
-            <x-course-tags :tagsCsv="$course->tags" />
-            <div class="text-xl font-bold mb-4">{{ $course->description }}</div>
-        </div>
+
+<!-- component -->
+<div class="min-h-screen bg-gradient-to-tr from-red-300 to-yellow-200 flex justify-center items-center py-20">
+    <div class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0">
+        <x-card />
+        <x-card />
+        <x-card />
+
+
+
+
     </div>
-</x-card>
+</div>
