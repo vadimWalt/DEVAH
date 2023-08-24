9a8d303d35948fe39e86e250883826c840273a25
h3ska <alaskar.hamidi@gmail.com>
2023-08-24 08:09:43 +0200

    controllers models routes and views

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,7 @@ class User extends Authenticatable
         'zip_code',
         'street',
         'country',
+        'role',
     ];
 
     /**
---
+++ resources/views/users/manage.blade.php
@@ -0,0 +1,59 @@
+<x-layout>
+    <div class="p-10 max-w-lg mx-auto mt-24">
+        <header class="text-center">
+            <h2 class="text-2xl font-bold uppercase mb-1">
+                Manage my account
+            </h2>
+        </header>
+
+        <form action="/users" method="POST">
+            @csrf {{-- don't forget ;) --}}
+            <div class="mb-6">
+                <label for="name" class="inline-block text-lg mb-2">
+                    Name
+                </label>
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name"
+                    value="{{ old('name') }}" />
+                @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <label for="email" class="inline-block text-lg mb-2">Email</label>
+                <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email"
+                    value="{{ old('email') }}" />
+                @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <label for="password" class="inline-block text-lg mb-2">
+                    Password
+                </label>
+                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password"
+                    value="{{ old('password') }}" />
+                @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <label for="password2" class="inline-block text-lg mb-2">
+                    Confirm Password
+                </label>
+                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password_confirmation" />
+                @error('password_confirmation')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+                <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
+                    Sign Up
+                </button>
+            </div>
+        </form>
+    </div>
+</x-layout>
