8291f42047bf9978876e71390db91449bd6bd4aa
h3ska <alaskar.hamidi@gmail.com>
2023-08-29 13:28:11 +0200

    commented out custom made css for testing

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="{{asset('styles/style.css') }}"> <!-- Use asset() to generate correct URL -->
+    <link rel="stylesheet" href="{{-- {{asset('styles/style.css') }} --}}"> <!-- Use asset() to generate correct URL -->
     {{-- cdn for alpine use for flash message for example --}}
     <script src="//unpkg.com/alpinejs" defer></script>
 
