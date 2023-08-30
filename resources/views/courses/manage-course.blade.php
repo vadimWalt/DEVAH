aa9af27f17f10776d3717afb7ed55c8e79771a03
h3ska <alaskar.hamidi@gmail.com>
2023-08-30 09:25:58 +0200

    teacher create course btn

--- resources/views/courses/manage-course.blade.php
+++ resources/views/courses/manage-course.blade.php
@@ -5,11 +5,20 @@
             My Courses
         </h1>
         <!-- Create course button -->
+        @if (auth()->user()->role=="teacher")
         <div class="text-center">
             <a href="{{ route('courses.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded">
                 Create Course
             </a>
         </div>
+        @else
+        <div class="text-center">
+            <a href="{{ route('courses.index') }}" class="bg-blue-500 text-white px-4 py-2 rounded">
+                Enroll to a course
+            </a>
+        </div>
+        @endif
+        
     </header>
     <!-- Table to display courses -->
     <table class="w-full table-auto rounded-sm">
